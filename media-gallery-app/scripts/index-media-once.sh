ddf0021fcf73ad08026f93bf29944fad523734e7
/home/raytr <113358673+siraytr@users.noreply.github.com>
2025-09-04 17:10:02 +0200

    Delete media-gallery-app directory

--- media-gallery-app/scripts/index-media-once.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Führt initialen Media-Scan via server/src/media/indexer.js
-set -euo pipefail
-
-# Annahme: node ist installiert und server Abhängigkeiten sind installiert
-pushd server
-node src/indexer-runner.js
-popd
-echo "Initialer Indexing-Run abgeschlossen."
--- media-gallery-app/server/src/media/allowed.js
+++
@@ -1,5 +0,0 @@
-// Welche Dateiendungen/MIME wir akzeptieren
-module.exports = {
-  IMAGE: ['.heic', '.heif', '.jpg', '.jpeg', '.png', '.webp'],
-  VIDEO: ['.mp4', '.mov', '.m4v']
-};
