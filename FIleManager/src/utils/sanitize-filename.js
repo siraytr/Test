9633e45ce57b475509bae6d56e4ddb2638e65f04
/home/raytr <113358673+siraytr@users.noreply.github.com>
2025-09-06 09:33:35 +0200

    Delete FIleManager directory

--- FIleManager/src/utils/sanitize-filename.js
+++
@@ -1,7 +0,0 @@
-const sanitize = require('sanitize-filename');
-module.exports = function safeName(name){
-  // ensure no path traversal and safe characters
-  const base = name.split('/').map(part => sanitize(part)).filter(Boolean).join('/');
-  // fallback
-  return base || 'file';
-};
